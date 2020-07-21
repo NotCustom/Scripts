a1e2833eeef4be799304b8d620898d7e24f0e7fa
Custom <65845579+NotCustom@users.noreply.github.com>
2020-07-20 21:06:37 -0500

    Update Ftf_ComputerESP.lua
    - No longer have to re-execute each time
    - Changed how updating works
    - More comments!

--- Flee The Facility/Ftf_ComputerESP.lua
+++ Flee The Facility/Ftf_ComputerESP.lua
@@ -3,17 +3,11 @@ uis = game:GetService("UserInputService")
 Key = "Q" -- yesyes you can change this
 HighlightColour = Color3.new(200,0,0) -- Highlight colour for computers
 HLTransparency = 0.5 -- How visible highlight is (0 = Solid Colour, 1 = Invisible)
--- Maps
-FACILITY_0 = workspace:FindFirstChild("Facility_0 by MrWindy")
-ABANDONED_PRISON = workspace:FindFirstChild("Abandoned Prison by AtomixKing and Duck_Ify")
-ABANDONED_FACILITY = workspace:FindFirstChild("Abandoned Facility by iiGalaxyKoala, Vexhins, and cyrda")
-HOMESTEAD = workspace:FindFirstChild("Homestead by MrWindy")
-AIRPORT = workspace:FindFirstChild("Airport by deadlybones28")
 -- Main stuff
 function update()
     -- originally this was a YandereDev style, massive `if then else` statement
     -- luckily, "or" can be used in locals 
-    local cmap = FACILITY_0 or ABANDONED_PRISON or ABANDONED_FACILITY or AIRPORT or HOMESTEAD
+    local cmap = workspace:FindFirstChild("Facility_0 by MrWindy") or workspace:FindFirstChild("Abandoned Prison by AtomixKing and Duck_Ify") or workspace:FindFirstChild("Abandoned Facility by iiGalaxyKoala, Vexhins, and cyrda") or workspace:FindFirstChild("Airport by deadlybones28") or workspace:FindFirstChild("Homestead by MrWindy")
     if cmap then
         for _,stuff in pairs(cmap:GetChildren()) do
             if stuff.Name == "ComputerTable" then
@@ -26,9 +20,10 @@ function update()
         end
     end
 end
-uis.InputBegan:connect(function(input)
+uis.InputBegan:connect(function(input) -- less atrocious coding
     if input.KeyCode == Enum.KeyCode[Key] then
-        local map = FACILITY_0 or ABANDONED_PRISON or ABANDONED_FACILITY or AIRPORT or HOMESTEAD
+        -- yesyes that looks worse now i know 
+        local map = workspace:FindFirstChild("Facility_0 by MrWindy") or workspace:FindFirstChild("Abandoned Prison by AtomixKing and Duck_Ify") or workspace:FindFirstChild("Abandoned Facility by iiGalaxyKoala, Vexhins, and cyrda") or workspace:FindFirstChild("Airport by deadlybones28") or workspace:FindFirstChild("Homestead by MrWindy")
         if map then
             for _,stuffs in pairs(map:GetChildren()) do
                 if stuffs.Name == "ComputerTable" then
@@ -49,4 +44,10 @@ uis.InputBegan:connect(function(input)
         end
     end
 end)
-game.ReplicatedStorage.ComputersLeft.Changed:connect(update)
+while wait(15) do -- adjust if needed
+    if game.ReplicatedStorage.IsGameActive.Value == true then
+        update()
+        -- "crafty" way to fix ESP not updating after exit was unlocked
+        -- considered binding to RenderStepped but forgot how s u p e r fast it is
+    end
+end
