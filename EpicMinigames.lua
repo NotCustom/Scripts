7bad76a55cbf3c6bcac64b948d891fbf6b48c45b
Custom <65845579+NotCustom@users.noreply.github.com>
2020-07-26 15:58:52 -0500

    poggers

--- EpicMinigames.lua
+++ EpicMinigames.lua
@@ -6,7 +6,14 @@ NEW = require(game:GetService("ReplicatedStorage").LoadLibrary.RbxUtility).Creat
 -- ^ parent parts to minigame map so it gets removed with map
 function topos(x,y,z)
     plr.Character.HumanoidRootPart.CFrame = CFrame.new(x,y,z)
-    -- existence made easier!
+end
+function topart(object,addpos) -- addpos takes Vector3 only :(((
+    -- yES THIS MAKES THINGS EASIER WHY AM I JUST NOW ADDING IT
+    if addpos then
+        plr.Character.HumanoidRootPart.CFrame = object.CFrame + addpos
+    else
+        plr.Character.HumanoidRootPart.CFrame = object.CFrame
+    end
 end
 function ping(Header,Msg)
     game.StarterGui:SetCore("SendNotification", {
@@ -26,14 +33,14 @@ uis.InputBegan:connect(function(input)
                     child:Destroy()
                 end
             end
-            --plr.Character.HumanoidRootPart.CFrame = mine[""].CFrame - CFrame.new(4,0,0)
         end
+        topart(workspace["Manic Mining"].emerald,Vector3.new(4,0,0))
         if workspace:FindFirstChild("Only One Path") then
             if workspace["Only One Path"].mapn.Value == "" then
                 topos(28.2, 85.8, -0.1)
             end
             if workspace["Only One Path"].mapn.Value == "Ritual" then
-                
+                topos(32.7396469, 105.910927, 12.1931028)
             end
         end
         if workspace:FindFirstChild("Turntable Scurry") then
@@ -72,13 +79,7 @@ uis.InputBegan:connect(function(input)
             topos(44.7786, 133.691, -45.1812)
         end
         if workspace:FindFirstChild("Shark Survival") then
-            if workspace["Shark Survival"].mapn.Value == "Winter" then
-            	topos(159.385, 87.3824, -24.1924)
-            end
-            if workspace["Shark Survival"].mapn.Value == "" then
-            	topos(183.032318, 74.5308151, -57.0457191)
-            	-- default map
-            end
+            topart(workspace["Shark Survival"].Goal.Goal)
         end
         if workspace:FindFirstChild("Laser Guidance") then
             workspace["Laser Guidance"].vlaser:Destroy()
@@ -130,15 +131,18 @@ uis.InputBegan:connect(function(input)
             -- i hate having to try and make this work
         end
         if workspace:FindFirstChild("Four Corners") then
-            NEW'Part'{Parent = workspace['Four Corners'],Name = 'PLATFORM',Size = Vector3.new(17,1,17),Position = Vector3.new(),Anchored = true}
-            -- position also needed here 
+            NEW'Part'{Parent = workspace['Four Corners'],Name = 'PLATFORM',Size = Vector3.new(17,1,17),Position = Vector3.new(-12.3369036, 69, -20.2035656),Anchored = true}
+            topos(-12.3369036, 72.0999985, -20.2035656)
         end
         if workspace:FindFirstChild("Teamwork Trial") then
-            topos()
-            -- kinda need position doe
+            -- sadchamp
         end
         if workspace:FindFirstChild("Avalanche") then
-            topos(-12.3015432, 37.4725342, -183.86824)
+            workspace.Avalanche.ChildAdded:connect(function(child)
+                if child.Name == "snowball" then
+                    child:Destroy()
+                end
+            end)
         end
         if workspace:FindFirstChild("Pyre Pit") then
             topos(15.9796066, 60.8599358, 1.58988118)
@@ -189,6 +193,58 @@ uis.InputBegan:connect(function(input)
                     highlight.Transparency = 0
                 end
             end
+        end
+        if workspace:FindFirstChild("Outbreak") then
+            topos(94.5852432, 72.6298981, 50.105278)
+        end
+        if workspace:FindFirstChild("Desert Dehydration") then
+            -- no clue if this is working because i had to try and fix it
+            -- "Handle is not a valid member of Tool" pretty sure it is but ok
+            local colas = workspace["Desert Dehydration"].bloxycolas
+            colas.DescendantAdded:connect(function(descendant)
+                if descendant:IsA("Part") then
+                    descendant.Handle.CFrame = plr.Character.HumanoidRootPart.CFrame
+                    -- yeah this is stupid
+                    -- ping me if it doesnt work @Custom#5556
+                end
+            end)
+        end
+        if workspace:FindFirstChild("High-Rolling") then
+            topart(workspace["High-Rolling"].Goal.Goal)
+        end
+        if workspace:FindFirstChild("Tile Takeover") then
+            if plr.Character.Head.teamDot.ImageLabel.ImageColor3 == Color3.fromRGB(255, 55, 55) then
+                repeat
+                    wait(0.001)
+                    for _,tiles in pairs(workspace["Tile Takeover"].tiles:GetChildren()) do
+                        if tiles.Color ~= Color3.fromRGB(196, 40, 28) then
+                            plr.Character.HumanoidRootPart.CFrame = tiles.CFrame
+                        end
+                    end
+                until string.match(workspace.notification.Value,"wins")
+                -- hopefully this will work
+            end
+            if plr.Character.Head.teamDot.ImageLabel.ImageColor3 == Color3.fromRGB(55, 55, 255) then
+                repeat
+                    wait(0.001)
+                    for _,tiles in pairs(workspace["Tile Takeover"].tiles:GetChildren()) do
+                        if tiles.Color ~= Color3.fromRGB(13, 105, 172) then
+                            plr.Character.HumanoidRootPart.CFrame = tiles.CFrame
+                        end
+                    end
+                until string.match(workspace.notification.Value,"wins")
+            end
+        end
+        if workspace:FindFirstChild("Balcony Bolt") then
+            -- more of a huge head start because of how rng it is
+            -- will do for now
+            topos(-721.234558, -7.84061432, -34.0784378)
+        end
+        if workspace:FindFirstChild("Conveyor Conundrum") then
+            workspace["Conveyor Conundrum"].obsts.ChildAdded:connect(function(child)
+                child:Destroy()
+            end)
+            -- what
         end
     end
 end)
