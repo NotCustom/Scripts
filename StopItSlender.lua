91d4c5d94356a8fea661c49886ea43243be8e611
Custom <65845579+NotCustom@users.noreply.github.com>
2020-09-06 20:27:18 -0500

    Update StopItSlender.lua
    Yes now you can activate generators from ∞ metres away!

--- StopItSlender.lua
+++ StopItSlender.lua
@@ -1,7 +1,8 @@
 -- did i mention you can change these?
 PageKey = "Q"
-GenKey = "E"
--- im literally reusing a slightly modified version of the ESPUpdate function from my FtF scripts what the hell
+HGenKey = "E"
+ActGenKey = "X" -- Actives generators instead of highlighting them
+-- still working on removing highlights for collected pages
 function updateGens()
     if workspace:FindFirstChild("MAP") then
         for _,gens in pairs(workspace.MAP.GENS:GetChildren()) do
@@ -13,13 +14,12 @@ function updateGens()
         end
     end 
 end
-
 game:GetService("UserInputService").InputBegan:connect(function(input)
     if input.KeyCode == Enum.KeyCode[PageKey] then
         local map = workspace:FindFirstChild("MAP")
         if map then
             for _,pages in pairs(map.Pages:GetChildren()) do    
-                if pages:IsA("Part") then
+                if pages:IsA("Part") and pages:FindFirstChild("Decal") then
                     local highlight = Instance.new("BoxHandleAdornment",pages)
                     highlight.Name = "SHHighlight"
                     highlight.Adornee = pages
@@ -30,17 +30,19 @@ game:GetService("UserInputService").InputBegan:connect(function(input)
                     highlight.Transparency = 0.3
                 end
             end
-            local hpagehighlight = Instance.new("BoxHandleAdornment",map.HPageSpawns.HPageSpawn)
-            hpagehighlight.Name = "SHHighlight"
-            hpagehighlight.Adornee = map.HPageSpawns.HPageSpawn
-            hpagehighlight.ZIndex = 10
-            hpagehighlight.AlwaysOnTop = true
-            hpagehighlight.Size = map.HPageSpawns.HPageSpawn.Size
-            hpagehighlight.Color3 = Color3.fromRGB(255,27,100)
-            hpagehighlight.Transparency = 0.3
+            if map.HPageSpawns.HPageSpawn:FindFirstChild("Decal") then
+                local hpagehighlight = Instance.new("BoxHandleAdornment",map.HPageSpawns.HPageSpawn)
+                hpagehighlight.Name = "SHHighlight"
+                hpagehighlight.Adornee = map.HPageSpawns.HPageSpawn
+                hpagehighlight.ZIndex = 10
+                hpagehighlight.AlwaysOnTop = true
+                hpagehighlight.Size = map.HPageSpawns.HPageSpawn.Size
+                hpagehighlight.Color3 = Color3.fromRGB(255,27,100)
+                hpagehighlight.Transparency = 0.3
+            end
         end
     end
-    if input.KeyCode == Enum.KeyCode[GenKey] then
+    if input.KeyCode == Enum.KeyCode[HGenKey] then
         local map = workspace:FindFirstChild("MAP")
         local generators = map.GENS
         if map then
@@ -55,6 +57,16 @@ game:GetService("UserInputService").InputBegan:connect(function(input)
                     highlight.Color3 = Color3.fromRGB(255,27,100)
                     highlight.Transparency = 0.3
                 end
+            end
+        end
+    end
+    if input.KeyCode == Enum.KeyCode[ActGenKey] then
+        local map = workspace:FindFirstChild("MAP")
+        local generators = map.GENS
+        for _,gens in pairs(generators:GetDescendants()) do -- GetDescendants is easier in cases like this
+            if gens:IsA("ClickDetector") then
+                gens.MaxActivationDistance = math.huge
+                fireclickdetector(gens)
             end
         end
     end
