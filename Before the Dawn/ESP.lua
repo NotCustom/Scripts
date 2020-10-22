b71ec6db019233026f4711a52ab35a5effd945fe
Custom <65845579+NotCustom@users.noreply.github.com>
2020-10-22 16:11:49 -0500

    Now with tags!

--- Before the Dawn/ESP.lua
+++ Before the Dawn/ESP.lua
@@ -1,8 +1,9 @@
 local key = "X" -- whatever key you like
 local colour = Color3.new(250,250,250) -- highlight colour | I recommend light ones like white
 local transparency = 0.3 -- 1 is completely invisible, 0 is solid
+local tags = false -- nametag for what slasher(*the thing cough cough*)
 game:GetService("UserInputService").InputBegan:connect(function(input)
-    if input.KeyCode == Enum.KeyCode[key] then
+    if input.KeyCode == Enum.KeyCode[key] then 
         local slasher
         for _,slashers in pairs(workspace.main.killers:GetChildren()) do
             if workspace:FindFirstChild(tostring(slashers.Name)) then
@@ -24,6 +25,24 @@ game:GetService("UserInputService").InputBegan:connect(function(input)
                         highlight.Size = limbs.Size
                         highlight.Color3 = colour
                         highlight.Transparency = transparency
+                        if tags == true then
+                            local board = Instance.new("BillboardGui",slasher.Head)
+                            local tag = Instance.new("TextLabel",board)
+                            board.Adornee = slasher.Head
+                            board.Name = "SenNametag_"..slasher.Name
+                            board.Size = UDim2.new(0,100,0,150)
+                            board.StudsOffset = Vector3.new(0,1,0)
+                            board.AlwaysOnTop = true
+                            tag.BackgroundTransparency = 1
+                            tag.Position = UDim2.new(0, 0, 0, -50)
+                            tag.Size = UDim2.new(0, 100, 0, 100)
+                            tag.Font = Enum.Font.SourceSansSemibold
+                            tag.TextSize = 20
+                            tag.TextColor3 = Color3.new(245, 245, 245)
+                            tag.TextStrokeTransparency = 0
+                            tag.TextYAlignment = Enum.TextYAlignment.Bottom
+                            tag.Text = slasher.Name
+                        end
                     end
                 end
             end)
