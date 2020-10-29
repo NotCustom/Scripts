d0bab5f870f6ff7e13bb271c03d0ae9d62bdadfe
Custom <65845579+NotCustom@users.noreply.github.com>
2020-10-29 12:44:46 -0500

    Added new map

--- Flee The Facility/Ftf_SavePeople.lua
+++ Flee The Facility/Ftf_SavePeople.lua
@@ -1,10 +1,10 @@
 local plr = game.Players.LocalPlayer
 local uis = game:GetService("UserInputService")
-Key = "Q"
+Key = "X"
 uis.InputBegan:connect(function(input)
     if input.KeyCode == Enum.KeyCode[Key] then 
         local pos = {}
-        local map = workspace:FindFirstChild("Facility_0 by MrWindy") or workspace:FindFirstChild("Abandoned Prison by AtomixKing and Duck_Ify") or workspace:FindFirstChild("Abandoned Facility by iiGalaxyKoala, Vexhins, and cyrda") or workspace:FindFirstChild("Airport by deadlybones28") or workspace:FindFirstChild("Homestead by MrWindy")
+        local map = workspace:FindFirstChild("Facility_0 by MrWindy") or workspace:FindFirstChild("Abandoned Prison by AtomixKing and Duck_Ify") or workspace:FindFirstChild("Abandoned Facility by iiGalaxyKoala, Vexhins, and cyrda") or workspace:FindFirstChild("Airport by deadlybones28") or workspace:FindFirstChild("Homestead by MrWindy") or workspace:FindFirstChild("The Library by Drainhp")
         if map then
             for _,things in pairs(map:GetChildren()) do
                 if things.Name == "FreezePod" then
