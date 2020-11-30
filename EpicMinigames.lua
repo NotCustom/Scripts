a4c1c647f8dbdd93f4f3db0f331a23c106d074ad
Custom <65845579+NotCustom@users.noreply.github.com>
2020-11-30 16:25:21 -0600

    Update???
    So many new things oh boy
    Had to remove a lot of stuff until I can update them

--- EpicMinigames.lua
+++ EpicMinigames.lua
@@ -2,18 +2,10 @@
 -- please never call me this again
 uis = game:GetService("UserInputService")
 plr = game.Players.LocalPlayer
-NEW = require(game:GetService("ReplicatedStorage").LoadLibrary.RbxUtility).Create
--- ^ parent parts to minigame map so it gets removed with map
-function topos(x,y,z)
-    plr.Character.HumanoidRootPart.CFrame = CFrame.new(x,y,z)
-end
-function topart(object,addpos) -- addpos takes Vector3 only :(((
-    -- yES THIS MAKES THINGS EASIER WHY AM I JUST NOW ADDING IT
-    if addpos then
-        plr.Character.HumanoidRootPart.CFrame = object.CFrame + addpos
-    else
-        plr.Character.HumanoidRootPart.CFrame = object.CFrame
-    end
+key = "Q" -- do i have to explain at this point
+function topart(object)
+    -- You cant teleport anymore, so just teleport it to you lol
+    object.CFrame = plr.Character.HumanoidRootPart.CFrame
 end
 function ping(Header,Msg)
     game.StarterGui:SetCore("SendNotification", {
@@ -24,9 +16,8 @@ function ping(Header,Msg)
 end
 
 uis.InputBegan:connect(function(input)
-    if input.KeyCode == Enum.KeyCode.Q then
+    if input.KeyCode == Enum.KeyCode[key] then
         if workspace:FindFirstChild("Manic Mining") then
-            print("what do i do lord\ndestroy the child\ncorrupt them all")
             local mine = workspace["Manic Mining"]
             for _,child in pairs(mine:GetChildren()) do
                 if child.Name == "dirt" or child.Name == "dirts" then
@@ -35,47 +26,19 @@ uis.InputBegan:connect(function(input)
             end
         end
         if workspace:FindFirstChild("Only One Path") then
-            if workspace["Only One Path"].mapn.Value == "" then
-                topos(28.2, 85.8, -0.1)
-            end
-            if workspace["Only One Path"].mapn.Value == "Ritual" then
-                topos(32.7396469, 105.910927, 12.1931028)
-            end
-        end
-        if workspace:FindFirstChild("Turntable Scurry") then
-            topos(0, 115.4, 0)
-        end
-        if workspace:FindFirstChild("Expedition Sprint") then
-            if workspace["Expedition Sprint"].mapn.Value == "High Peak" then
-                topos(50.0934715, 255.104126, -5.76367521)
-            end
-        end
-        if workspace:FindFirstChild("Compactor Crash") then
-            topos(51.5406075, 95.1099854, -46.3891144)
+            topart(workspace["Only One Path"].Goal.Goal)
         end
         if workspace:FindFirstChild("Hedge Way Out") then
             workspace["Hedge Way Out"].hedges:Destroy()
         end
         if workspace:FindFirstChild("Lane Jumping") then
-            topos(59.0051155, 72.0980911, 123.205139)
-        end
-        if workspace:FindFirstChild("Lava Forecast") then
-        	topos(-99.1484222, 66.4740677, -21.999424)
+            topart(workspace["Lane Jumping"].Goal.Goal)
         end
         if workspace:FindFirstChild("Penguin Pushover") then
         	workspace["Penguin Pushover"].penguins:Destroy()
         end
         if workspace:FindFirstChild("Bullet Evasion") then
         	workspace["Bullet Evasion"].bullets:Destroy()
-        end
-        if workspace:FindFirstChild("Blox City Brawl") then
-            topos(23.2213268, 115.508118, -187.013397) 
-        end
-        if workspace:FindFirstChild("Aztec Adventure") then
-            topos(-575.656982, 31.6598053, -27.1059475)
-        end
-        if workspace:FindFirstChild("Bullet Bound") then
-            topos(44.7786, 133.691, -45.1812)
         end
         if workspace:FindFirstChild("Shark Survival") then
             topart(workspace["Shark Survival"].Goal.Goal)
@@ -87,15 +50,12 @@ uis.InputBegan:connect(function(input)
         if workspace:FindFirstChild("Trial Traversing") then
             topart(workspace["Trial Traversing"].Goal.Goal)
         end
-        if workspace:FindFirstChild("The Crusher") then
-        	topos(-32.4165611, -100.058777, -52.3167686)
-        end
         if workspace:FindFirstChild("Crystal Clear") then
             local igems = workspace["Crystal Clear"].igems
             local hasgem = false
             igems.ChildAdded:connect(function(child)
                 if hasgem == false then
-                   plr.Character.HumanoidRootPart.CFrame = child.CFrame
+                   topart(child)
                    hasgem = true
                 end
             end)
@@ -106,23 +66,8 @@ uis.InputBegan:connect(function(input)
         if workspace:FindFirstChild("The Sweeper") then
             workspace["The Sweeper"].sweeper:Destroy()
         end
-        if workspace:FindFirstChild("Revolution Cube") then
-            NEW'Part'{Parent = workspace["Revolution Cube"],Name = "PLATFORM",Size = Vector3.new(17,1,17),Position = Vector3.new(129.284, 114.654, -946.287),Anchored = true}
-            topos(129.284, 116.654, -946.287)
-        end
         if workspace:FindFirstChild("Just Jump") then
            workspace["Just Jump"].map.rope:Destroy() 
-        end
-        if workspace:FindFirstChild("Uncertain Ground") then
-            NEW'Part'{Parent = workspace["Uncertain Ground"],Name = "Platform",Size = Vector3.new(17,1,17),Position = Vector3.new(19.6044769, 98.766319, -128.549683),Anchored = true}
-            topos(19.6044769, 98.766319, -128.549683)
-        end
-        if workspace:FindFirstChild("Cake Delivery") then
-            -- i hate having to try and make this work
-        end
-        if workspace:FindFirstChild("Four Corners") then
-            NEW'Part'{Parent = workspace['Four Corners'],Name = 'PLATFORM',Size = Vector3.new(17,1,17),Position = Vector3.new(-12.3369036, 69, -20.2035656),Anchored = true}
-            topos(-12.3369036, 72.0999985, -20.2035656)
         end
         if workspace:FindFirstChild("Teamwork Trial") then
             topart(workspace["Teamwork Trial"].goal) -- finally
@@ -130,12 +75,10 @@ uis.InputBegan:connect(function(input)
         if workspace:FindFirstChild("Avalanche") then
             workspace.Avalanche.ChildAdded:connect(function(child)
                 if child.Name == "snowball" then
-                    child:Destroy()
+                    child.Size = Vector3.new(0,0,0)
+                    -- cant delete it, that gets noticed 
                 end
             end)
-        end
-        if workspace:FindFirstChild("Pyre Pit") then
-            topos(15.9796066, 60.8599358, 1.58988118)
         end
         if workspace:FindFirstChild("Compactor Crash") then
             workspace["Compactor Crash"].crusher:Destroy()
@@ -143,30 +86,23 @@ uis.InputBegan:connect(function(input)
         if workspace:FindFirstChild("Hard-Pressed") then
             workspace["Hard-Pressed"].map.presses:Destroy()
         end
-        if workspace:FindFirstChild("Romper Stomper") then
-            if not string.match(workspace.notification.Value,plr.Name) then
-                topos(156.021225, 43, -200.100769)
-            end
-        end
         if workspace:FindFirstChild("Mechanical Mayhem") then
-            workspace["Mechanical Mayhem"].map.cogs.mainspinner:Destroy()
-        end
-        if workspace:FindFirstChild("Cliffside Chaos") then
-            topos(8.58114815, 293.600891, -38.3512726)
+            workspace["Mechanical Mayhem"].map.turn:Destroy()
         end
         if workspace:FindFirstChild("Block Hunt") then
-            -- please let this be the final version of this one
+            -- Final rewrite @ 30.7.2020
             local redblocks = workspace["Block Hunt"].DroppedRedBlocks
             local blueblocks = workspace["Block Hunt"].DroppedBlueBlocks
-            for _,blues in pairs(blueblocks:GetChildren()) do
-                blues.CFrame = plr.Character.HumanoidRootPart.CFrame
+            if plr.Character.Head.teamDot.ImageLabel.ImageColor3 == Color3.fromRGB(55,55,255) then
+                for _,reds in pairs(redblocks:GetChildren()) do
+                    reds.CFrame = plr.Character.HumanoidRootPart.CFrame
+                end
             end
-            for _,reds in pairs(redblocks:GetChildren()) do
-                reds.CFrame = plr.Character.HumanoidRootPart.CFrame
+            if plr.Character.Head.teamDot.ImageLabel.ImageColor3 == Color3.fromRGB(255,55,55) then
+                for _,blues in pairs(blueblocks:GetChildren()) do
+                    blues.CFrame = plr.Character.HumanoidRootPart.CFrame
+                end
             end
-        end
-        if workspace:FindFirstChild("Outbreak") then
-            topos(94.5852432, 72.6298981, 50.105278)
         end
         if workspace:FindFirstChild("Desert Dehydration") then
             -- if you arent in round and get the sodas with this
@@ -184,10 +120,10 @@ uis.InputBegan:connect(function(input)
         if workspace:FindFirstChild("Tile Takeover") then
             if plr.Character.Head.teamDot.ImageLabel.ImageColor3 == Color3.fromRGB(255, 55, 55) then
                 repeat
-                    wait(0.001)
+                    wait(0.00001)
                     for _,tiles in pairs(workspace["Tile Takeover"].tiles:GetChildren()) do
                         if tiles.Color ~= Color3.fromRGB(196, 40, 28) then
-                            plr.Character.HumanoidRootPart.CFrame = tiles.CFrame + Vector3.new(0,2,0)
+                            topart(tiles)
                         end
                     end
                 until string.match(workspace.notification.Value,"wins")
@@ -195,35 +131,51 @@ uis.InputBegan:connect(function(input)
             end
             if plr.Character.Head.teamDot.ImageLabel.ImageColor3 == Color3.fromRGB(55, 55, 255) then
                 repeat
-                    wait(0.001)
+                    wait(0.00001)
                     for _,tiles in pairs(workspace["Tile Takeover"].tiles:GetChildren()) do
                         if tiles.Color ~= Color3.fromRGB(13, 105, 172) then
-                            plr.Character.HumanoidRootPart.CFrame = tiles.CFrame + Vector3.new(0,2,0)
+                            topart(tiles)
                         end
                     end
                 until string.match(workspace.notification.Value,"wins")
             end
-        end
-        if workspace:FindFirstChild("Balcony Bolt") then
-            -- more of a huge head start because of how rng it is
-            -- will do for now
-            topos(-721.234558, -7.84061432, -34.0784378)
         end
         if workspace:FindFirstChild("Conveyor Conundrum") then
             workspace["Conveyor Conundrum"].obsts.ChildAdded:connect(function(child)
                 child:Destroy()
             end)
         end
-        if workspace:FindFirstChild("Crumble Island") then
-            NEW'Part'{Name = "PLATFORM",Parent = workspace["Crumble Island"],Size = Vector3.new(17,1,17),Position = Vector3.new(52.658844, 45.5821228, -50.265358),Anchored = true}
-            topos(52.658844, 48.5821228, -50.265358)
-        end
         if workspace:FindFirstChild("Gear Battle") then
             workspace["Gear Battle"].map.ignore.ChildAdded:connect(function(child)
                 if child:IsA("Part") then
-                    child.Position = plr.Character.HumanoidRootPart.Position
+                    child.CFrame = plr.Character.HumanoidRootPart.CFrame
                 end
             end)
+        end
+        if workspace:FindFirstChild("Haunted Hallways") then
+            topart(workspace["Haunted Hallways"].Goal.Goal)
+        end
+        if workspace:FindFirstChild("Castle Climb") then
+            topart(workspace["Castle Climb"].Goal)
+        end
+        if workspace:FindFirstChild("Spiky Spinner") then
+            workspace["Spiky Spinner"].SpikySpinner:Destroy()
+        end
+        if workspace:FindFirstChild("Protect the Statue") then
+            for _,tnts in pairs(workspace["Protect the Statue"].tnts:GetChildren()) do
+                tnts.ChildAdded:connect(function(child)
+                    if child.Name == "nfusep" then
+                        topart(child)
+                    end
+                end)
+            end
+        end
+        if workspace:FindFirstChild("Crag Climber") then
+            topart(workspace["Crag Clamber"].Goal)
+        end
+        if workspace:FindFirstChild("Spiral Ascent") then
+            workspace["Spiral Ascent"].FinishPart.RemoteFunction:InvokeServer()
+            -- aha, you fools !
         end
     end
 end)
