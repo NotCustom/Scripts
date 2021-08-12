b173a5b58474858de6e4410256a3db4eef81fc93
Custom <65845579+NotCustom@users.noreply.github.com>
2021-08-11 23:47:32 -0500

    lol

---
+++ NoAnomalyAM.lua
@@ -0,0 +1,8 @@
+-- I hate the Anomaly in a similar way to how I hate the Xenomorph in Alien: Isolation
+-- Unfortunately I can't solve the latter issue this easily :(
+    
+workspace.Camera.Anomaly:Destroy()
+workspace.Camera.laserBeam:Destroy()
+game.StarterPlayer.StarterCharacterScripts.AnomalyRunner:Destroy()
+game:GetService("ReplicatedStorage").Anomaly:Destroy()
+workspace.Players.CustomCoded:WaitForChild("AnomalyRunner"):Destroy()
