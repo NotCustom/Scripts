0e8ae0d46fafdf5c8f75e412e174bf9c6955cf82
Custom <65845579+NotCustom@users.noreply.github.com>
2021-08-24 18:43:46 -0500

    remove certain things and added stuff

--- OreFinderAM.lua
+++ OreFinderAM.lua
@@ -1,7 +1,8 @@
 -- Highlights ores listed in the table, useful for finding specific things
+-- Use _G.Add() and _G.Remove() to update the ore list instead of re-executing or rejoining the game
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NotCustom/Scripts/master/Resource/OreColourList.lua", true))() -- Loads my custom ore colour table
 
-local ores = {
+_G.ores = {
     "Antimatter",
     "Painite"
 }
@@ -9,6 +10,12 @@ local settings = {
     HighlightTransparency = 0.5,
     Update = true
 }
+_G.Add = function(ore)
+    table.insert(_G.ores, tostring(ore))
+end)
+_G.Remove = function(ore)
+    table.remove(ore, table.find(ores, tostring(ore)))
+end)
 function Highlight(ore)
     local highlight = Instance.new("BoxHandleAdornment", ore)
     highlight.Adornee = ore
@@ -23,13 +30,13 @@ end
 -- Main stuff
 
 for _,oress in pairs(workspace.Mine:GetChildren()) do
-    if table.find(ores, oress.Name) then
+    if table.find(_G.ores, oress.Name) then
         Highlight(oress)
     end
 end
 if settings.Update == true then
     workspace.Mine.ChildAdded:connect(function(child)
-        if table.find(ores, child.Name) then
+        if table.find(_G.ores, child.Name) then
             Highlight(child)
         end
     end)
