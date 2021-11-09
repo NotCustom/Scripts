6b7c6601e98eef57548e6ea5236a60de486118e5
NotCustom <puprblx@gmail.com>
2021-11-08 18:33:19 -0600

    revert to fix

--- Flee The Facility/Ftf_SavePeople.lua
+++ Flee The Facility/Ftf_SavePeople.lua
@@ -1,28 +1,24 @@
--- i have a backup of this in the event that it doesn't work
--- will test later
 local plr = game.Players.LocalPlayer
 local uis = game:GetService("UserInputService")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NotCustom/Scripts/master/Resource/FtF_Maps.lua"))() -- summons the great old ones via table containing all the maps
 Key = "X"
 uis.InputBegan:connect(function(input)
     if input.KeyCode == Enum.KeyCode[Key] then 
         local pos = {}
-        for _,map in pairs(_G.Maps) then
-            if workspace:FindFirstChild(map) then
-                for _,things in pairs(workspace[map]:GetChildren()) do
-                    if things.Name == "FreezePod" then
-                        if things:FindFirstChild("PodTrigger") then
-                            if things.PodTrigger.CapturedTorso.Value ~= nil then
-                                table.insert(pos,things.PodTrigger.CFrame)
-                                things.PodTrigger.CFrame = plr.Character.HumanoidRootPart.CFrame
-                                wait(1)
-                                things.PodTrigger.CFrame = pos[1]
-                                pos = nil
-                            end
+        local map = workspace:FindFirstChild("Facility_0 by MrWindy") or workspace:FindFirstChild("Abandoned Prison by AtomixKing and Duck_Ify") or workspace:FindFirstChild("Abandoned Facility by iiGalaxyKoala, Vexhins, and cyrda") or workspace:FindFirstChild("Airport by deadlybones28") or workspace:FindFirstChild("Homestead by MrWindy") or workspace:FindFirstChild("The Library by Drainhp")
+        if map then
+            for _,things in pairs(map:GetChildren()) do
+                if things.Name == "FreezePod" then
+                    if things:FindFirstChild("PodTrigger") then
+                        if things.PodTrigger.CapturedTorso.Value ~= nil then
+                            table.insert(pos,things.PodTrigger.CFrame)
+                            things.PodTrigger.CFrame = plr.Character.HumanoidRootPart.CFrame
+                            wait(1)
+                            things.PodTrigger.CFrame = pos[1]
+                            pos = nil
                         end
                     end
                 end
             end
-        end 
+        end
     end
 end)
