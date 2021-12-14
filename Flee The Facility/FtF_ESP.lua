ca68c2615ad824d339e19ecfbacd61b207c0b8f9
NotCustom <puprblx@gmail.com>
2021-12-14 13:42:26 -0600

    updoot

--- Flee The Facility/FtF_ESP.lua
+++ Flee The Facility/FtF_ESP.lua
@@ -1,56 +1,34 @@
--- I went through here, because I was aware the code was messy
--- I misunderstood how bad the situation was.
--- Thank god a lot was reworked
+-- i have no clue why vscode had a seizure last time i tried to upload this
+-- :hollow:
 plr = game.Players.LocalPlayer
 uis = game:GetService("UserInputService")
-<<<<<<< HEAD
--- YES YOU CAN CUSTOMIZE IT
--- SOMETHING EVERYONE WANTS
-Key = "Q" -- no spaces if its something like "LeftControl"
+Key = "Q"
 BeastHighlight = Color3.fromRGB(200,0,0)
 SurvivorHighlight = Color3.fromRGB(0,200,0)
 HighlightTransparency = 0.5
--- actual functions
-function Highlight(role, folderloc, thefunny)
-=======
-Key = "Q" 
--- Functions
-function Highlight(role,rolecolour,folderloc,funny) -- funny is useless but 
-    -- Coincidentally folderloc = Character, so just reuse
->>>>>>> f8e3560a4ffbaf5aa63cc6ca7b45d1d03c0cf38b
+agony = false
+
+function Highlight(role, thefunny)
     spawn(function()
-        if not folderloc:FindFirstChild("MDSHighlight") then
-            local file = Instance.new("Folder",folderloc)
-            file.Name = "MDSHighlight"
+        if not game.CoreGui.EndUserLicenseAgreement:FindFirstChild("E_Highlight_"..thefunny.Name) then
+            local file = Instance.new("Folder",game.CoreGui.EndUserLicenseAgreement)
+            file.Name = "E_Highlight_"..thefunny.Name
             local bboard = Instance.new("BillboardGui",file)
             local tag = Instance.new("TextLabel",bboard)
-<<<<<<< HEAD
-            for _,limbs in pairs(folderloc.Character:GetChildren()) do
-=======
-            for _,limbs in pairs(folderloc:GetChildren()) do
->>>>>>> f8e3560a4ffbaf5aa63cc6ca7b45d1d03c0cf38b
+            for _,limbs in pairs(thefunny:GetChildren()) do
                 if limbs:IsA("Part") then
                     local highlight = Instance.new("BoxHandleAdornment",file)
-                    highlight.Name = "MDSHighlight_"..limbs.Name
+                    highlight.Name = "EHl_"..limbs.Name
                     highlight.Adornee = limbs
                     highlight.ZIndex = 10
                     highlight.AlwaysOnTop = true
                     highlight.Size = limbs.Size
-<<<<<<< HEAD
                     highlight.Color3 = SurvivorHighlight
                     highlight.Transparency = HighlightTransparency
                 end
             end
             bboard.Adornee = them.Character.Head
             bboard.Name = "MDSNametag_"..them.Name
-=======
-                    highlight.Color3 = rolecolour
-                    highlight.Transparency = 0.5
-                end
-            end
-            bboard.Adornee = folderloc.Head
-            bboard.Name = "MDSNametag_"..funny.Name
->>>>>>> f8e3560a4ffbaf5aa63cc6ca7b45d1d03c0cf38b
             bboard.Size = UDim2.new(0,100,0,150)
             bboard.StudsOffset = Vector3.new(0,1,0)
             bboard.AlwaysOnTop = true
@@ -62,43 +40,32 @@ function Highlight(role,rolecolour,folderloc,funny) -- funny is useless but
             tag.TextColor3 = Color3.new(245, 245, 245)
             tag.TextStrokeTransparency = 0
             tag.TextYAlignment = Enum.TextYAlignment.Bottom
-<<<<<<< HEAD
             tag.Text = "Name: "..thefunny.Name.." - "..role
-=======
-            tag.Text = "Name: "..funny.Name.." - "..role
->>>>>>> f8e3560a4ffbaf5aa63cc6ca7b45d1d03c0cf38b
         end
     end)
 end
+
 uis.InputBegan:connect(function(input)
     if input.KeyCode == Enum.KeyCode[Key] then
-        if game.ReplicatedStorage.IsGameActive.Value == true then
-            for _,them in pairs(game.Players:GetChildren()) do
-                if them.Character:FindFirstChild("Hammer") and them.Name ~= plr.Name then
-                    -- BEAST HIGHLIGHT
-<<<<<<< HEAD
-                    Highlight("Beast", them.Character, them)
-                elseif not them.Character:FindFirstChild("Hammer") and them.Name ~= plr.Name then
-                    -- SURVIVOR HIGHLIGHT
-                    Highlight("Survivor", them.Character, them)
-=======
-                    Highlight("Beast", Color3.fromRGB(200,0,0), them.Character, them)
-                elseif not them.Character:FindFirstChild("Hammer") and them.Name ~= plr.Name then
-                    -- SURVIVOR HIGHLIGHT
-                    Highlight("Survivor", Color3.fromRGB(0,200,0), them.Character, them)
->>>>>>> f8e3560a4ffbaf5aa63cc6ca7b45d1d03c0cf38b
+        if not agony then
+            agony = true
+            if game.ReplicatedStorage.IsGameActive.Value == true then
+                for _,them in pairs(game.Players:GetChildren()) do
+                    if them.Character:FindFirstChild("Hammer") and them.Name ~= plr.Name then
+                        Highlight("Beast", them.Character, them)
+                    elseif not them.Character:FindFirstChild("Hammer") and them.Name ~= plr.Name then
+                        Highlight("Survivor", them.Character, them)
+                    end
                 end
             end
+        else
+            agony = false
+            game.CoreGui.EndUserLicenseAgreement:ClearAllChildren()
         end
     end
 end)
 game.ReplicatedStorage.IsGameActive.Changed:connect(function(value)
     if value == false then
-        for _,peoples in pairs(game.Players:GetChildren()) do
-            if peoples.Character:FindFirstChild("MDSHighlight") then
-                peoples.Character.MDSHighlight:Destroy()
-                -- finally !
-            end
-        end
+        game.CoreGui.EndUserLicenseAgreement:ClearAllChildren()
     end
 end)
--- Flee The Facility/Ftf_ComputerESP.lua
+++ Flee The Facility/Ftf_ComputerESP.lua
@@ -1,40 +1,32 @@
 uis = game:GetService("UserInputService")
 Key = "Q"
-HighlightColour = Color3.new(200,0,0) -- Highlight colour for computers
-HLTransparency = 0.5 -- How visible highlight is (0 = Solid Colour, 1 = Invisible)
+HighlightColour = Color3.new(200,0,0)
+HLTransparency = 0.5 
+everynighticanhearscreamsfromthewoodsinmybackyardandidontknowwhattodo = false 
 -- Main stuff
-function update()
-    local cmap = workspace:FindFirstChild("Facility_0 by MrWindy") or workspace:FindFirstChild("Abandoned Prison by AtomixKing and Duck_Ify") or workspace:FindFirstChild("Abandoned Facility by iiGalaxyKoala, Vexhins, and cyrda") or workspace:FindFirstChild("Airport by deadlybones28") or workspace:FindFirstChild("Homestead by MrWindy") or workspace:FindFirstChild("The Library by Drainhp") 
-    if cmap then
-        for _,stuff in pairs(cmap:GetChildren()) do
-            if stuff.Name == "ComputerTable" then
-                if stuff.Screen.Color == Color3.fromRGB(40, 127, 71) then
-                    if stuff:FindFirstChild("MDSHighlight") then
-                        stuff.MDSHighlight:Destroy()
-                    end
-                end
-            end
-        end
-    end
-end
 uis.InputBegan:connect(function(input)
     if input.KeyCode == Enum.KeyCode[Key] then
-        local map = workspace:FindFirstChild("Facility_0 by MrWindy") or workspace:FindFirstChild("Abandoned Prison by AtomixKing and Duck_Ify") or workspace:FindFirstChild("Abandoned Facility by iiGalaxyKoala, Vexhins, and cyrda") or workspace:FindFirstChild("Airport by deadlybones28") or workspace:FindFirstChild("Homestead by MrWindy") or workspace:FindFirstChild("The Library by Drainhp") 
+        if not everynighticanhearscreamsfromthewoodsinmybackyardandidontknowwhattodo then everynighticanhearscreamsfromthewoodsinmybackyardandidontknowwhattodo = true else everynighticanhearscreamsfromthewoodsinmybackyardandidontknowwhattodo = false end
+        local map = workspace:FindFirstChild(tostring(game.ReplicatedStorage.CurrentMap.Value))
         if map then
             spawn(function()
                 for _,stuffs in pairs(map:GetChildren()) do
                     if stuffs.Name == "ComputerTable" then
-                        if stuffs.Screen.Color ~= Color3.fromRGB(40, 127, 71) then
-                            if not stuffs:FindFirstChild("MDSHighlight") then
-                                local highlight = Instance.new("BoxHandleAdornment",stuffs)
-                                highlight.Name = "MDSHighlight"
-                                highlight.Adornee = stuffs.BasePart
-                                highlight.ZIndex = 10
-                                highlight.AlwaysOnTop = true
-                                highlight.Size = stuffs.BasePart.Size
-                                highlight.Color3 = HighlightColour
-                                highlight.Transparency = HLTransparency
+                        if everynighticanhearscreamsfromthewoodsinmybackyardandidontknowwhattodo then
+                            if stuffs.Screen.Color ~= Color3.fromRGB(40, 127, 71) then
+                                if not stuffs:FindFirstChild("MDSHighlight") then
+                                    local highlight = Instance.new("BoxHandleAdornment",stuffs)
+                                    highlight.Name = "MDSHighlight"
+                                    highlight.Adornee = stuffs.BasePart
+                                    highlight.ZIndex = 10
+                                    highlight.AlwaysOnTop = true
+                                    highlight.Size = stuffs.BasePart.Size
+                                    highlight.Color3 = HighlightColour
+                                    highlight.Transparency = HLTransparency
+                                end
                             end
+                        else
+                            stuffs:FindFirstChild("MDSHighlight"):Destroy()
                         end
                     end
                 end
@@ -42,10 +34,20 @@ uis.InputBegan:connect(function(input)
         end
     end
 end)
-while wait(15) do -- adjust if needed
-    if game.ReplicatedStorage.IsGameActive.Value == true then
-        update()
-        -- "crafty" way to fix ESP not updating after exit was unlocked
-        -- considered binding to RenderStepped but forgot how s u p e r fast it is
+
+game.ReplicatedStorage.TimeRemaining.Value.Changed:connect(function(value)
+    if value > 0 and game.ReplicatedStorage.IsGameActive.Value == true then -- Round is 15m/900seconds, so 885 seconds minus the headstart
+        local cmap = workspace:FindFirstChild(tostring(game.ReplicatedStorage.CurrentMap.Value))
+        if cmap then
+            for _,stuff in pairs(cmap:GetChildren()) do
+                if stuff.Name == "ComputerTable" then
+                    if stuff.Screen.Color == Color3.fromRGB(40, 127, 71) then
+                        if stuff:FindFirstChild("MDSHighlight") then
+                            stuff.MDSHighlight:Destroy()
+                        end
+                    end
+                end
+            end
+        end
     end
-end
+end)
