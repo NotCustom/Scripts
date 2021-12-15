63138d55a712e635dc466d41f62fc366b356c24a
Custom <65845579+NotCustom@users.noreply.github.com>
2021-12-15 16:14:50 -0600

    amongus

--- Flee The Facility/Ftf_ComputerESP.lua
+++ Flee The Facility/Ftf_ComputerESP.lua
@@ -1,32 +1,27 @@
 uis = game:GetService("UserInputService")
 Key = "Q"
 HighlightColour = Color3.new(200,0,0)
-HLTransparency = 0.5 
-everynighticanhearscreamsfromthewoodsinmybackyardandidontknowwhattodo = false 
+HLTransparency = 0.5
+youbuyprostitutesforsexibuyprostitutestofeedtheskinwalkersinmybackyardwearenotthesame = false -- set to true to not fail computers
 -- Main stuff
 uis.InputBegan:connect(function(input)
     if input.KeyCode == Enum.KeyCode[Key] then
-        if not everynighticanhearscreamsfromthewoodsinmybackyardandidontknowwhattodo then everynighticanhearscreamsfromthewoodsinmybackyardandidontknowwhattodo = true else everynighticanhearscreamsfromthewoodsinmybackyardandidontknowwhattodo = false end
         local map = workspace:FindFirstChild(tostring(game.ReplicatedStorage.CurrentMap.Value))
         if map then
             spawn(function()
                 for _,stuffs in pairs(map:GetChildren()) do
                     if stuffs.Name == "ComputerTable" then
-                        if everynighticanhearscreamsfromthewoodsinmybackyardandidontknowwhattodo then
-                            if stuffs.Screen.Color ~= Color3.fromRGB(40, 127, 71) then
-                                if not stuffs:FindFirstChild("MDSHighlight") then
-                                    local highlight = Instance.new("BoxHandleAdornment",stuffs)
-                                    highlight.Name = "MDSHighlight"
-                                    highlight.Adornee = stuffs.BasePart
-                                    highlight.ZIndex = 10
-                                    highlight.AlwaysOnTop = true
-                                    highlight.Size = stuffs.BasePart.Size
-                                    highlight.Color3 = HighlightColour
-                                    highlight.Transparency = HLTransparency
-                                end
+                        if stuffs.Screen.Color ~= Color3.fromRGB(40, 127, 71) then
+                            if not stuffs:FindFirstChild("MDSHighlight") then
+                                local highlight = Instance.new("BoxHandleAdornment",stuffs)
+                                highlight.Name = "MDSHighlight"
+                                highlight.Adornee = stuffs.BasePart
+                                highlight.ZIndex = 10
+                                highlight.AlwaysOnTop = true
+                                highlight.Size = stuffs.BasePart.Size
+                                highlight.Color3 = HighlightColour
+                                highlight.Transparency = HLTransparency
                             end
-                        else
-                            stuffs:FindFirstChild("MDSHighlight"):Destroy()
                         end
                     end
                 end
@@ -49,5 +44,12 @@ while wait(15) do -- adjust if needed
 				end
 			end
 		end
+    end
+end
+
+if youbuyprostitutesforsexibuyprostitutestofeedtheskinwalkersinmybackyardwearenotthesame then
+    while true do
+        game.ReplicatedStorage.RemoteEvent:FireServer("SetPlayerMinigameResult", true)
+        wait()
     end
 end
